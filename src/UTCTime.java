bd87baa7d3890c82bc15df92123b3d9c3dbfd70d
tjx <1002376198@qq.com>
2019-09-25 12:30:20 +0800

    utcTime

---
+++ src/UTCTime.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class UTCTime {
+    private LocalDateTime standardTime;
+
+    public LocalDateTime getStandardTime() {
+        return standardTime;
+    }
+
+    public void setStandardTime(LocalDateTime standardTime) {
+        this.standardTime = standardTime;
+    }
+}

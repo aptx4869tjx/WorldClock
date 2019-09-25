f7673b772aaccdffccb369f810e8cfc75a2fab53
tjx <1002376198@qq.com>
2019-09-25 23:16:23 +0800

    test result

---
+++ src/main/java/PhoneTime.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+public class PhoneTime extends Watched {
+    private LocalDateTime phoneTime;
+    public PhoneTime() {
+        this.phoneTime = LocalDateTime.now();
+    }
+
+    public LocalDateTime getPhoneTime() {
+        return phoneTime;
+    }
+
+    public void setPhoneTime(LocalDateTime phoneTime) {
+        this.phoneTime = phoneTime;
+        notifyAllWatchers(this.getPhoneTime());
+    }
+}
